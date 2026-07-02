c6be8c4ead68c1f9e48bd6baaf017458eae80778
Mesafint12prince <mesafintgetnet12@gmail.com>
2026-07-02 23:13:07 +0300

    completed Script generator

--- modules/script_generator.py
+++ modules/script_generator.py
@@ -1,24 +1,76 @@
-from providers.gemini_provider import generate
+import os
+
+from providers import ProviderFactory
 
 from prompts.youtube_prompt import SYSTEM_PROMPT
 
-import os
 
+class ScriptGenerator:
 
-def create_script(topic):
+    def __init__(self):
 
-    script = generate(
-        SYSTEM_PROMPT,
-        f"Topic:\n{topic}"
-    )
+        self.provider = ProviderFactory.create()
 
-    os.makedirs("output", exist_ok=True)
+    def generate(
+        self,
+        topic: str
+    ) -> str:
 
-    filename = topic.lower().replace(" ", "_")
+        user_prompt = f"""
+Topic:
+{topic}
+"""
 
-    filepath = f"output/{filename}_script.txt"
+        script = self.provider.generate(
+            SYSTEM_PROMPT,
+            user_prompt
+        )
 
-    with open(filepath, "w", encoding="utf-8") as file:
-        file.write(script)
+        return script
 
-    return filepath
+    def save(
+        self,
+        topic: str,
+        script: str
+    ) -> str:
+
+        os.makedirs(
+            "output",
+            exist_ok=True
+        )
+
+        filename = (
+            topic.lower()
+            .replace(" ", "_")
+            + "_script.txt"
+        )
+
+        filepath = os.path.join(
+            "output",
+            filename
+        )
+
+        with open(
+            filepath,
+            "w",
+            encoding="utf-8"
+        ) as file:
+
+            file.write(script)
+
+        return filepath
+
+    def generate_and_save(
+        self,
+        topic: str
+    ):
+
+        script = self.generate(topic)
+        
+        filepath = self.save(
+            topic,
+            script
+        )
+
+        return script, filepath
+    
